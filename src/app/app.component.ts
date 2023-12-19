2c0d0b6857ed137815cf9bace883f39bd68952b6
Silvestre525 <silvestrevillar02@gmail.com>
2023-12-19 01:51:00 -0300

    add services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JsonPlaceholderService } from './services/json-place-holder.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  placeHolder: any = {};
+
+  constructor(private service: JsonPlaceholderService) {}
+
+  ngOnInit(): void {
+    this.service.getAllJsonPlaceholder().subscribe((placeHolder: any) => {
+      this.placeHolder = placeHolder.result;
+      console.log(this.placeHolder);
+    });
+  }
+}
--- src/app/services/json-place-holder.service.ts
+++ src/app/services/json-place-holder.service.ts
@@ -10,21 +10,8 @@ export class JsonPlaceholderService {
 
   constructor(private http: HttpClient) { }
 
-  // Obtener posts paginados y filtrados por título
-  getPosts(page: number, title?: string): Observable<any[]> {
-    let params = new HttpParams()
-      .set('_page', String(page))
-      .set('_limit', '10');
-
-    if (title) {
-      params = params.set('title', title);
-    }
-
-    return this.http.get<any[]>(this.apiUrl, { params });
+public getAllJsonPlaceholder(): Observable<any>{
+  return this.http.get(this.apiUrl);
   }
 
-  // Crear un nuevo post
-  createPost(postData: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, postData);
-  }
 }
